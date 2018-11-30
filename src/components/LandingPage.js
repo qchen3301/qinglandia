c80632147935267f74d3dc9ae2926f11d7e47547
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-11-30 18:05:43 -0500

    linked portfolio.js to landingpage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 import styled from 'styled-components'
+import Portfolio from './Portfolio'
 
 const Banner = styled.div`
     display: flex;
@@ -26,25 +27,19 @@ export default class LandingPage extends Component {
       <div>
           <a href="#Body">
           <Banner> 
-              WELCOME TO QINGLANDIA
+                QINGLANDIA
           </Banner>
           </a>
         </div>
-          Hello from Landing Page
           <Body id="Body">
-              <h1>H E N T A I</h1>
               <h3>Hi. I'm Qing. I'm a junior developer. Recently I graduated from a three month 
                   immersive web development course at General Assembly. Here's a sample of 
                   some of the apps I've built in class. Each project was built in a week long agile sprint
                   with version control.  
               </h3>
-
-              Doge Slots<br/>
-              College!<br/>
-              The Doge Taroge<br/>
           </Body>
-          <div id="test">
-              Test test test test
+          <div>
+              <Portfolio/>
           </div>
       </div>
     )
