ab530ae70320d2551e88c79f3118269fd0bd3643
qchen3301 <q.chen.3301@gmail.com>
2018-12-04 02:43:49 -0500

    added package to project

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,11 +3,19 @@ import Portfolio from './Portfolio'
 import AboutMe from './AboutMe'
 import Contact from './Contact'
 import { Parallax, ParallaxLayer } from 'react-spring/dist/addons'
+import { Spring } from 'react-spring'
 import styled from 'styled-components'
 
-const LandingPageBackground = styled.div`
+// LandingPageForeground and LandingPageBackground work together to create a dynamically generated background
+//LandingPageForeground will also define the CSS for the hero banner "layer"
+const LandingPageForeground = styled.div`
   background-color: #20232f;
-  clip-path: polygon(75% 0%, 100% 50%, 100% 100%, 0% 100%, 74% 52%, 0% 0%);
+  clip-path: polygon(100% 0, 100% 50%, 100% 100%, 0% 100%, 100% 77%, 0% 0%);
+  height: 100vh;
+
+`
+const LandingPageBackground = styled.div`
+  background-color: green;
 `
 
 export default class LandingPage extends Component {
@@ -17,7 +25,11 @@ export default class LandingPage extends Component {
       {/* Splash Page/Hero Banner */}
         <ParallaxLayer offset={0} speed={0.5}>
           <LandingPageBackground onClick={()=> this.parallax.scrollTo(1)}>
-            Hi this is Qinglandia's landing page
+          <LandingPageForeground>
+            <Spring from={{opacity: 0}} to={{opacity:1}}/>
+              This be the landing page
+            
+          </LandingPageForeground>
           </LandingPageBackground>
         </ParallaxLayer>
         {/* About Me */}
