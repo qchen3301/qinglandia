361c3138d7bfd36bf65483f8d74b4a111682764c
qchen3301 <q.chen.3301@gmail.com>
2018-12-04 13:42:38 -0500

    abandoned idea of seperate compo for AboutMe, rolled it into LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,33 +1,58 @@
 import React, { Component } from 'react'
 import Portfolio from './Portfolio'
-import AboutMe from './AboutMe'
 import Contact from './Contact'
 import { Parallax, ParallaxLayer } from 'react-spring/dist/addons'
+import {Spring, config} from 'react-spring'
 import styled from 'styled-components'
 
-
-// LandingPageForeground and LandingPageBackground work together to create a dynamically generated background
-const LandingPageForeground = styled.div`
-  background-color: #20232f;
-  clip-path: polygon(100% 0, 100% 50%, 100% 100%, 0% 100%, 100% 77%, 0% 0%);
-  height: 100vh;
+const TextDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    margin-right: auto;
+    margin-top: 10%;
+    margin-left: 50%;
+    text-transform: uppercase;
 `
-const LandingPageBackground = styled.div`
-  background: rgb(201,222,150);
+
+const BackgroundDiv = styled.div`
+  background: linear-gradient(to right, SlateBlue 0%, DeepSkyBlue 100%);
+  height: 100vh;
+  width: 100vw;
 `
 export default class LandingPage extends Component {
   render() {
     return (
+      <BackgroundDiv>
       <Parallax className="container" pages={3} scrolling={true} vertical ref={ref=> this.parallax = ref}>
       {/* Splash Page/Hero Banner */}
-        <ParallaxLayer offset={0} speed={0.3}>
-          <LandingPageBackground onClick={()=> this.parallax.scrollTo(1)}>
-          <AboutMe/>
-          <LandingPageForeground></LandingPageForeground>
-          </LandingPageBackground>
+        <ParallaxLayer offset={0} speed={0.5}>
+        <TextDiv onClick={()=> this.parallax.scrollTo(1)}>  
+            <Spring config={config.default} from={{opacity:0}} to={{opacity:1}} reset={true}>
+              {props=> <div style={props}>Hi. I'm Qing. I'm a jr web developer.</div> }
+            </Spring>
+            <br/><br/>
+            <Spring config={config.default} from={{opacity:0}} to={{opacity:1}} reset={true} delay={3000}>
+              {props=> <div style={props}> I've recently finished a three month long <br/>coding bootcamp at General Assembly Atlanta.</div> }
+            </Spring>
+            <br/><br/>
+            <Spring config={config.default} from={{opacity:0}} to={{opacity:1}} reset={true} delay={6000}>
+              {props=> <div style={props}>Here are some of the projects I've built.<br/></div> }
+            </Spring>
+            <br/><br/>
+            <Spring config={config.default} from={{opacity:0}} to={{opacity:1}} reset={true} delay={9400}>
+              {props=> <div style={props}>Each app was built and deployed <br/>in agile development cycles <br/>using Git as version control</div> }
+            </Spring>
+            <br/><br/>
+            <Spring config={config.default} from={{opacity:0}} to={{opacity:1}} reset={true} delay={12000}>
+              {props=> <div style={props}>Click or scroll down to see my portfolio</div> }
+            </Spring>
+          </TextDiv> 
         </ParallaxLayer>
         {/* Projects */}
-        <ParallaxLayer offset={1} speed={0.3}>
+        <ParallaxLayer offset={1} speed={0.5}>
           <button onClick={()=> this.parallax.scrollTo(0)}>Click Me To Go Up</button>
           <button onClick={()=> this.parallax.scrollTo(2)}>click Me To Go To Contacts</button>
           These are the projects<br/>
@@ -40,6 +65,7 @@ export default class LandingPage extends Component {
           <Contact/>
         </ParallaxLayer>
       </Parallax>
+      </BackgroundDiv>
     )
   }
 }
