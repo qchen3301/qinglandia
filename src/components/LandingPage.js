fbacdb8d9d9a631861bb7799951395426d9e85df
qchen3301 <q.chen.3301@gmail.com>
2018-12-04 15:26:23 -0500

    deleted contact.js, combine with portfolio.js compo

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import Portfolio from './Portfolio'
-import Contact from './Contact'
 import { Parallax, ParallaxLayer } from 'react-spring/dist/addons'
 import {Spring, config} from 'react-spring'
 import styled from 'styled-components'
 
+const BackgroundDiv = styled.div`
+  background: linear-gradient(to right, SlateBlue 0%, DeepSkyBlue 100%);
+  height: 100vh;
+  width: 100vw;
+`
 const TextDiv = styled.div`
     display: flex;
     align-items: center;
@@ -16,19 +20,24 @@ const TextDiv = styled.div`
     margin-left: 50%;
     text-transform: uppercase;
 `
-
-const BackgroundDiv = styled.div`
-  background: linear-gradient(to right, SlateBlue 0%, DeepSkyBlue 100%);
-  height: 100vh;
-  width: 100vw;
+const CenterProjectCardsDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 40vh;
+  margin-left: 50%;
+  margin-right: 50%;
 `
+
 export default class LandingPage extends Component {
   render() {
     return (
       <BackgroundDiv>
-      <Parallax className="container" pages={3} scrolling={true} vertical ref={ref=> this.parallax = ref}>
+        Welcome to Qinglandia
+      <Parallax className="container" pages={2} scrolling={true} vertical ref={ref=> this.parallax = ref}>
       {/* Splash Page/Hero Banner */}
-        <ParallaxLayer offset={0} speed={0.5}>
+        <ParallaxLayer offset={0} speed={0.9}>
         <TextDiv onClick={()=> this.parallax.scrollTo(1)}>  
             <Spring config={config.default} from={{opacity:0}} to={{opacity:1}} reset={true}>
               {props=> <div style={props}>Hi. I'm Qing. I'm a jr web developer.</div> }
@@ -54,15 +63,8 @@ export default class LandingPage extends Component {
         {/* Projects */}
         <ParallaxLayer offset={1} speed={0.5}>
           <button onClick={()=> this.parallax.scrollTo(0)}>Click Me To Go Up</button>
-          <button onClick={()=> this.parallax.scrollTo(2)}>click Me To Go To Contacts</button>
           These are the projects<br/>
-          <Portfolio/>
-        </ParallaxLayer>
-        {/* Contact */}
-        <ParallaxLayer offset={2} speed={0.5}>
-          <button onClick={()=> this.parallax.scrollTo(0)}>Back To About Me</button>
-          <button onClick={()=> this.parallax.scrollTo(2)}>Back To Projects</button>
-          <Contact/>
+          <CenterProjectCardsDiv><Portfolio/></CenterProjectCardsDiv>
         </ParallaxLayer>
       </Parallax>
       </BackgroundDiv>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react'
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 
 const PortfolioBody = styled.div`
     display: flex;
-    align-items: center;
-    flex-direction: column;
+    align-items: space-between;
+    flex-direction: row;
     justify-content: center;
     font-family: 'Comic Sans MS';
-    font-size: 5vh;
-    background-color: blue;
-    height: 45vh;
+    /* height: 45vh; */
+    width: 55vw;
     text-decoration: none;
-    
+`
+const CardDiv =styled.div`
+  background: red;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  /* width: 40%; */
+  text-align: center;
+
 `
 
 export default class Portfolio extends Component {
   render() {
     return (
       <PortfolioBody>
-        Slots Game <br/>
-        College! <br/>
-        The Taroge <br/>
-        Qinglandia
+        <CardDiv>
+          <h4>Slots Game</h4>
+          <p>A slots game throwback to the late 90s era of website design</p>
+          </CardDiv>
+        <CardDiv>
+          <h4>College!</h4>
+          <p>A (sigh) to-do list app to help my little sister keep track of her application process to various colleges</p>
+        </CardDiv>
+          <CardDiv>
+            <h4>The Taroge</h4>
+            <p>The capstone project! A React-On-Rails app making external API calls on the back-end</p>
+          </CardDiv>
+          <CardDiv>
+            <h4>Qinglandia</h4>
+            <p>This website! Click on this card to go to the Github page</p>
+          </CardDiv>
       </PortfolioBody>
     )
   }
